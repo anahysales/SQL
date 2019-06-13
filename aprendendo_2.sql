10a21b7c17b2188d83b1bc9e66fadda53663b7a9
anahysales <anahy.sales@gmail.com>
2019-06-13 18:02:40 -0300

    Primeira aula sobre SQL

---
+++ aprendendo_2.sql
@@ -0,0 +1,19 @@
+use aprendendo_2;
+INSERT INTO alunos2 (
+	nome, 
+	sobrenome, 
+	email,
+	data_nascimento, 
+	cpf
+) VALUES (
+	"João", 
+	"Teste", 
+	"joão@gmail.com", 
+	"1998-05-22", 
+	"12345678559"
+);
+
+
+
+CREATE TABLE 
+
